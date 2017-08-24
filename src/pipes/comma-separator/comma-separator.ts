dddc8138929e503259c70026b70d7ba46a33b1d7
scrpgil <scraplbuild@gmail.com>
2017-08-24 19:22:57 +0900

    -になると表示がおかしくなるのを修正

--- src/pipes/comma-separator/comma-separator.ts
+++ src/pipes/comma-separator/comma-separator.ts
@@ -5,8 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CommaSeparatorPipe implements PipeTransform {
     transform(value: string, ...args) {
+        if(value == ""){
+            return;
+        }
+        var tmp = String(Math.abs(Number(value)));
+        var minus = false;
+        if(Number(value) < 0){
+            minus = true;
+        }
         var ret ="";
-        var res = value.split(".");
+        var res = tmp.split(".");
         for(var i = 1;i <= res[0].length;i++){
             if((i % 3) == 0){
                 if(res[0][res[0].length - (i + 1)]){
@@ -20,6 +28,9 @@ export class CommaSeparatorPipe implements PipeTransform {
         }
         if(res.length >= 2){
             ret = ret + "." + res[1]; 
+        }
+        if(minus){
+            ret = "-" + ret;
         }
         return ret;
     }

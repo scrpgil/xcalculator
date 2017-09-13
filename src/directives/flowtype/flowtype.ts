ae6c4d2fb2540b748f86d718b4dc0b2a55a569a3
scrpgil <scraplbuild@gmail.com>
2017-09-14 00:52:10 +0900

    flowtypeのInput名変更対応2

--- src/directives/flowtype/flowtype.ts
+++ src/directives/flowtype/flowtype.ts
@@ -4,8 +4,8 @@ import { OnChanges, Directive, ElementRef, Input } from '@angular/core';
     selector: '[flowtype]' // Attribute selector
 })
 export class FlowtypeDirective implements OnChanges {
-    @Input('flowtype') flowType: any;
-    @Input('size') size: number;
+    @Input('flowtype') flowtype: any;
+    @Input() size: number;
     constructor(private el: ElementRef) {
     }
 
@@ -15,17 +15,18 @@ export class FlowtypeDirective implements OnChanges {
         this.changeFontSize();
     }
     ngOnChanges(changes:any) {
+        console.log("change");
         this.changeFontSize();
     }
     changeFontSize(){
-        console.log("change:" + this.size );
+        console.log("change:" + this.size + ", text:" + this.flowtype);
         var fontSize = this.size;
-        var len = this.fontType.length - 1;
+        var len = this.flowtype.length - 1;
         if(this.size > (100 / len) * 1.7){
             var ret = (100 / len) * 1.7;
             fontSize = ret;
         }
         this.el.nativeElement.style.fontSize = fontSize + "vw";
-        this.el.nativeElement.innerHTML = this.flowType;
+        this.el.nativeElement.innerHTML = this.flowtype;
     }
 }
